2ebc6a835d4756e43f49dfb6ead16e625f899da1
Mehmettt <87609530+Mehmetgul0202@users.noreply.github.com>
2021-10-27 21:24:52 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
 
 anlik_calisan = []
 
-@client.on(events.NewMessage(pattern='^(?i)/cancel@UtagOnline_bot'))
+@client.on(events.NewMessage(pattern='^(?i)/cancel@MissTagBot'))
 async def cancel(event):
   global anlik_calisan
   anlik_calisan.remove(event.chat_id)
@@ -27,21 +27,21 @@ async def cancel(event):
 async def start(event):
   await event.reply("**Etiketleme Botu**, Grup veya kanaldaki neredeyse tüm üyelerden bahsedebilir ★\nDaha fazla bilgi için **/help**'i tıklayın.",
                     buttons=(
-                      [Button.url('🌟 Beni Bir Gruba Ekle', 'https://t.me/UtagOnline_bot?startgroup=a'),
-                      Button.url('music botu', 'https://t.me/esmermusic_bot'),
-                      Button.url('👮‍♂️ Sahibim', 'https://t.me/baykaoss')]
+                      [Button.url('🌟 Beni Bir Gruba Ekle', 'https://t.me/MissTagBot?startgroup=a'),
+                      Button.url('music botu', 'https://t.me/missmusicsbot'),
+                      Button.url('👮‍♂️ Sahibim', 'https://t.me/MissKraL')]
                     ),
                     link_preview=False
                    )
 @client.on(events.NewMessage(pattern="^/help$"))
 async def help(event):
-  helptext = """**UtagOnline Tag Bot'un Yardım Menüsü**\n\nKomut: /all \n  Bu komutu, başkalarına bahsetmek istediğiniz metinle birlikte kullanabilirsiniz. \n`Örnek: /all Günaydın!`  \nBu komutu yanıt olarak kullanabilirsiniz. herhangi bir mesaj Bot, yanıtlanan iletiye kullanıcıları etiketleyerek ve /bitir yazarak etiketleme işlemi biter.
+  helptext = """**Miss Tag Bot'un Yardım Menüsü**\n\nKomut: /all \n  Bu komutu, başkalarına bahsetmek istediğiniz metinle birlikte kullanabilirsiniz. \n`Örnek: /all Günaydın!`  \nBu komutu yanıt olarak kullanabilirsiniz. herhangi bir mesaj Bot, yanıtlanan iletiye kullanıcıları etiketleyerek ve /bitir yazarak etiketleme işlemi biter.
 \nAdminleri etiketlemek için ise /admin \n komutunu kullanabilirsiniz sevgiler"""
   await event.reply(helptext,
                     buttons=(
-                      [Button.url('🌟 Beni Bir Gruba Ekle', 'https://t.me/UtagOnline_bot?startgroup=a'),
-                       Button.url('📣 kanal', 'https://t.me/UtagOnline'),
-                      Button.url('👮‍♂️ Sahibim', 'https://t.me/baykaoss')]
+                      [Button.url('🌟 Beni Bir Gruba Ekle', 'https://t.me/MİSStagBot?startgroup=a'),
+                       Button.url('📣 kanal', 'https://t.me/MissMusicSupport'),
+                      Button.url('👮‍♂️ Sahibim', 'https://t.me/MissKraL')]
                     ),
                     link_preview=False
                    )
@@ -173,5 +173,5 @@ async def mentionalladmin(event):
     return await event.respond("__Bu komut gruplarda ve kanallarda kullanılabilir.!__")
   
 
-print(">> Bot çalıyor merak etme 👮‍♂️ @UtagOnline bilgi alabilirsin <<")
+print(">> Bot çalıyor merak etme 👮‍♂️ @MissTagBot bilgi alabilirsin <<")
 client.run_until_disconnected()
